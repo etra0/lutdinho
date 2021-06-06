c92c3a2342fa54d36d554b95854ebf03bb1ae35d
Sebastián Aedo <sebastian.aedo@sansano.usm.cl>
2021-06-06 01:41:03 -0400

    Created image generator for cube

---
+++ src/cube.rs
@@ -0,0 +1,78 @@
+use image::ImageBuffer;
+use regex::Regex;
+use std::num::ParseFloatError;
+use std::path::Path;
+use std::io::{self, BufReader, BufRead};
+
+pub struct Cube {
+    pub size: usize,
+    pub values: Vec<Color>
+}
+
+#[derive(Debug)]
+pub struct Color (f32, f32, f32);
+
+impl Cube {
+    pub fn parse<P: AsRef<Path>>(filepath: P) -> Result<Cube, Box<dyn std::error::Error>> {
+        let size_regex = Regex::new(r"LUT_3D_SIZE (\d*)")?;
+        let value_regex = Regex::new(r"(\d\.\d+) (\d\.\d+) (\d\.\d+)")?;
+        let mut buf = String::new();
+        let mut cube_file = BufReader::new(std::fs::File::open(filepath)?);
+        let mut cube = Cube { size: 0, values: vec![] };
+
+        while let Ok(read) = cube_file.read_line(&mut buf) {
+            if let Some(capture) = size_regex.captures(&buf) {
+                cube.size = capture[1].parse()?;
+                break;
+            }
+
+            if read == 0 {
+                return Err("file doesn't contains LUT_3D_SIZE".into())
+            }
+
+            buf.clear();
+        }
+
+        while let Ok(read) = cube_file.read_line(&mut buf) {
+            if let Some(cap) = value_regex.captures(&buf) {
+                cube.values.push(Color(
+                    cap[1].parse()?,
+                    cap[2].parse()?,
+                    cap[3].parse()?,
+                ));
+            }
+
+            if read == 0 {
+                break;
+            }
+
+            buf.clear();
+        }
+
+        if (cube.values.len() % cube.size) != 0_usize {
+            return Err(format!("Current values aren't divisible by size: {} {}", cube.values.len(),  cube.size).into());
+        }
+
+        Ok(cube)
+    }
+
+    pub fn generate_image(&self) -> image::RgbImage {
+        let width = self.values.len() / self.size;
+        let mut img = ImageBuffer::new(width as u32, self.size as u32);
+
+        for (i, pixel) in self.values.iter().enumerate() {
+            let s = i / (self.size * self.size);
+            let x = (i % self.size) + self.size * s;
+            let y = (i / self.size) % self.size;
+            img.put_pixel(x as _, y as _, image::Rgb::<u8>::from(pixel));
+        }
+
+        return img;
+    }
+}
+
+impl From<&Color> for image::Rgb<u8> {
+    fn from(color: &Color) -> Self {
+        return image::Rgb([(color.0 * 255.) as u8, (color.1 * 255.) as u8, (color.2 * 255.) as u8]);
+    }
+}
---
+++ tests/cube.rs
@@ -0,0 +1,17 @@
+use lutdinho::cube::Cube;
+
+static CUBE_FILE: &str = std::concat!(std::env!("CARGO_MANIFEST_DIR"), "/tests/test_lut.cube");
+
+#[test]
+fn test_parse() {
+    let cube = Cube::parse(CUBE_FILE).unwrap();
+    assert_eq!(cube.size, 32);
+    assert_eq!(cube.values.len(), 32768);
+}
+
+#[test]
+fn test_image() {
+    let cube = Cube::parse(CUBE_FILE).unwrap();
+    let img = cube.generate_image();
+    img.save("output.png").unwrap();
+}
